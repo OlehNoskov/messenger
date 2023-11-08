50d474cd51d735c77ed6dac7a91be97a946ea6a6
noskov <olega6000@gmail.com>
2023-11-08 15:58:55 +0200

    Add test + 1

--- server/src/main/java/com/example/server/ServerApplication.java
+++ server/src/main/java/com/example/server/ServerApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ServerApplication {
 
     public static void main(String[] args) {
-        System.out.println("Test + 1");
+        System.out.println("Test + 2");
         SpringApplication.run(ServerApplication.class, args);
     }
 }
